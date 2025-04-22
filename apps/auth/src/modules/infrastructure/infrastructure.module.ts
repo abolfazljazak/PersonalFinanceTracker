fc7b3372fd9abdcc18f44777d972fbdfa4935cdd
abolfazl jazak <jazakabolfazl@gmail.com>
2025-04-22 22:57:34 +0330

    feat(auth): enhance InfrastructureModule and InfrastructureService for JWT and password management
    - Updated InfrastructureModule to include JwtModule for JWT authentication.
    - Added JwtService, PasswordService, and JwtStrategy as providers.
    - Enhanced InfrastructureService with methods for creating access tokens, hashing passwords, and validating passwords using the new services.

--- apps/auth/src/modules/infrastructure/infrastructure.module.ts
+++ apps/auth/src/modules/infrastructure/infrastructure.module.ts
@@ -1,4 +1,30 @@
 import { Module } from '@nestjs/common';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { PasswordService } from './services/password.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt/strategy/jwt.strategy';
+import { InfrastructureService } from './infrastructure.service';
 
-@Module({})
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('Jwt.accessTokenSecret'),
+        signOptions: {
+          expiresIn: configService.get<string>('Jwt.expire'),
+        },
+      }),
+    }),
+  ],
+  providers: [
+    JwtService,
+    PasswordService,
+    ConfigService,
+    JwtStrategy,
+    InfrastructureService,
+  ],
+  exports: [InfrastructureService],
+})
 export class InfrastructureModule {}
--- apps/auth/src/modules/infrastructure/infrastructure.service.ts
+++ apps/auth/src/modules/infrastructure/infrastructure.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
+import { TokenService } from './jwt/token.service';
+import { PasswordService } from './services/password.service';
+import { TokensPayload } from './jwt/types/payload';
 
 @Injectable()
-export class InfrastructureService {}
+export class InfrastructureService {
+  constructor(
+    private readonly tokenService: TokenService,
+    private readonly passwordService: PasswordService
+  ) {}
+
+  async createAccessToken(payload: TokensPayload): Promise<string> {
+    return this.tokenService.createAccessToken(payload)
+  }
+
+  async hashPassword(password: string): Promise<string> {
+    return this.passwordService.hashPassword(password)
+  }
+
+  async validatePassword(password: string, hashPassword: string): Promise<boolean> {
+    return this.passwordService.validatePassword(password, hashPassword)
+  }
+}
